d519ce71f7c7bbe9f8a8b59aa0d65e29602b107c
Adi-Pradhan <101237060+Adi-Pradhan@users.noreply.github.com>
2022-06-04 15:49:44 +0530

    Add files via upload

--- 35-OSA_Source/src/main/java/com/zensar/Application.java
+++ 35-OSA_Source/src/main/java/com/zensar/Application.java
@@ -2,8 +2,16 @@ package com.zensar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableWebMvc
 @SpringBootApplication
+@EnableEurekaClient
+@ComponentScan(basePackages = "com.zensar")
+@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
